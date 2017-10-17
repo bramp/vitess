64a87322eb93cf128fcc4b0825afec25949c18c8
Andrew Brampton <bramp@google.com>
2017-10-17 08:38:58 -0700

    Added more scanIdentifier tests, please changed the scanId, and scanString tests to ensure the tests are always fully scanned.

--- go/vt/sqlparser/token_test.go
+++ go/vt/sqlparser/token_test.go
@@ -21,12 +21,45 @@ import (
 	"testing"
 )
 
-func TestLiteralID(t *testing.T) {
+var (
+	// Simple mapping of token IDs to names. This should really
+	// be provided by the goyacc output, but it isn't.
+	// https://github.com/golang/go/issues/6577
+	tokNames = map[int]string{
+		STRING:    "STRING",
+		ID:        "ID",
+		0:         "EOF",
+		LEX_ERROR: "LEX_ERROR",
+	}
+)
+
+func tokenName(id int) string {
+	if name, found := tokNames[id]; found {
+		return name
+	}
+
+	return fmt.Sprintf("%d", id)
+}
+
+func TestID(t *testing.T) {
 	testcases := []struct {
-		in  string
-		id  int
-		out string
+		in     string
+		id     int
+		out    string
+		nextId int
 	}{{
+		in:  "a",
+		id:  ID,
+		out: "a",
+	}, {
+		in:  "abc",
+		id:  ID,
+		out: "abc",
+	}, {
+		in:  "a1b2",
+		id:  ID,
+		out: "a1b2",
+	}, {
 		in:  "`aa`",
 		id:  ID,
 		out: "aa",
@@ -39,9 +72,10 @@ func TestLiteralID(t *testing.T) {
 		id:  ID,
 		out: "a`b",
 	}, {
-		in:  "`a``b`c",
-		id:  ID,
-		out: "a`b",
+		in:     "`a``b`c",
+		id:     ID,
+		out:    "a`b",
+		nextId: ID,
 	}, {
 		in:  "`a``b",
 		id:  LEX_ERROR,
@@ -60,25 +94,20 @@ func TestLiteralID(t *testing.T) {
 		tkn := NewStringTokenizer(tcase.in)
 		id, out := tkn.Scan()
 		if tcase.id != id || string(out) != tcase.out {
-			t.Errorf("Scan(%s): %d, %s, want %d, %s", tcase.in, id, out, tcase.id, tcase.out)
+			t.Errorf("Scan(%q) = (%s, %q), want (%s, %q)", tcase.in, tokenName(id), out, tokenName(tcase.id), tcase.out)
+		}
+		if id, out = tkn.Scan(); id != tcase.nextId {
+			t.Errorf("Scan() = (%s, %q), want (%s, ?)", tokenName(id), out, tokenName(tcase.nextId))
 		}
 	}
 }
 
-func tokenName(id int) string {
-	if id == STRING {
-		return "STRING"
-	} else if id == LEX_ERROR {
-		return "LEX_ERROR"
-	}
-	return fmt.Sprintf("%d", id)
-}
-
 func TestString(t *testing.T) {
 	testcases := []struct {
-		in   string
-		id   int
-		want string
+		in     string
+		id     int
+		want   string
+		nextId int
 	}{{
 		in:   "''",
 		id:   STRING,
@@ -130,9 +159,14 @@ func TestString(t *testing.T) {
 	}}
 
 	for _, tcase := range testcases {
-		id, got := NewStringTokenizer(tcase.in).Scan()
+		tkn := NewStringTokenizer(tcase.in)
+		id, got := tkn.Scan()
 		if tcase.id != id || string(got) != tcase.want {
 			t.Errorf("Scan(%q) = (%s, %q), want (%s, %q)", tcase.in, tokenName(id), got, tokenName(tcase.id), tcase.want)
 		}
+		if id, got = tkn.Scan(); id != tcase.nextId {
+			t.Errorf("Scan() = (%s, %q), want (%s, ?)", tokenName(id), got, tokenName(tcase.nextId))
+		}
+
 	}
 }
